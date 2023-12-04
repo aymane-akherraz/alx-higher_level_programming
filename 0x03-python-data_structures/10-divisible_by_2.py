54c3004fc6fe71210dd596f373c938de6095fb62
Aymane Akherraz <aymaneakherraz@gmail.com>
2023-12-04 12:52:35 +0100

    Modified some files

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@ def divisible_by_2(my_list=[]):
     res = []
     for i in my_list:
         if i % 2:
-            res.append(0)
+            res.append(False)
         else:
-            res.append(1)
+            res.append(True)
     return res
