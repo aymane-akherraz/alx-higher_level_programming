c2202438f75e2f908a52a755da4373b418de5099
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-03-11 19:38:16 +0000

    Modified 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,30 +1,13 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-const list = [];
 if (argv.length <= 3) {
   console.log(0);
 } else {
+  const list = [];
   argv.forEach((e, i) => {
     if (i > 1) {
       list.push(parseInt(e));
     }
   });
-  let m = list[0];
-  const len = list.length;
-  let i = 1;
-  while (i < len) {
-    if (list[i] > m) {
-      m = list[i];
-    }
-    i++;
-  }
-  let sm = list[0] !== m ? list[0] : list[1];
-  i = 1;
-  while (i < len) {
-    if (list[i] > sm && list[i] !== m) {
-      sm = list[i];
-    }
-    i++;
-  }
-  console.log(sm);
+  console.log(list.sort(function (a, b) { return b - a; })[1]);
 }
