36646b2188cdbc7eac192eeb89efb2b027ff1ad3
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-03-12 16:33:27 +0000

    Modified some files

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,9 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (typeof w === 'number' && typeof h === 'number' &&
-        Number.isInteger(w) && Number.isInteger(h) &&
-        w > 0 && h > 0) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
