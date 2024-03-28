b93966e02a855dbf213bdfdc2973a8b0c6b8704a
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-03-28 02:57:34 +0000

    Created 0x10 and added some files

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Sends a GET request to the URL, and displays the body of the response
+if curl -sLI "$1" | grep -q "200"; then
+	curl -sL "$1"
+fi
---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+file_content=$(cat "$2")
+curl -sH "Content-Type: application/json" -d "$file_content" "$1"
